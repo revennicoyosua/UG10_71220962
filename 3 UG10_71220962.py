f690ece879118daf8db3db42aa330f3acdae41ca
revennicoyosua <118961107+revennicoyosua@users.noreply.github.com>
2022-11-24 16:40:03 +0700

    Add files via upload

---
+++ 3 UG10_71220962.py
@@ -0,0 +1,7 @@
+daftar = input("masukan daftar pesanan: ")
+pedagang = daftar.split()
+print("daftar pesanan : ",pedagang)
+nambah = input("masukan pesanan yang ingin ditambahkan")
+pedagang.append(nambah)
+print("hasil penambahan dalam pesanan: ",pedagang)
+
